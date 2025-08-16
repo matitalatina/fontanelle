6b0bdad85bfc1e7919da3055d1dd4539ad9cbf0b
Mattia Natali <matitalatina@gmail.com>
2025-08-16 14:30:05 +0200

    Add 'app' route to sitemap with priority 0.9

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,6 +4,7 @@ import { BASE_URL } from "./seo-config";
 // Define the routes with their relative priorities
 const routes = [
   { path: "", priority: 1.0 },
+  { path: "app", priority: 0.9 },
   { path: "legend", priority: 0.8 },
   { path: "credits", priority: 0.7 },
 ];
